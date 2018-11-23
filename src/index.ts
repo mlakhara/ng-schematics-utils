3fda55d4fd4d32f116e970bfdb6a3682d0f38dc9
Manish Lakhara <manishl@meditab.com>
2018-11-23 15:38:41 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// start:ng42.barrel
+export * from './ast-utils';
+export * from './change';
+export * from './config';
+export * from './dependencies';
+export * from './find-module';
+export * from './json-utils';
+export * from './latest-versions';
+export * from './lint-fix';
+export * from './ng-ast-utils';
+export * from './parse-name';
+export * from './project-targets';
+export * from './project';
+export * from './validation';
+export * from './workspace-models';
+export * from './test';
+// end:ng42.barrel
+
---
+++ src/latest-versions.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export const latestVersions = {
+  // These versions should be kept up to date with latest Angular peer dependencies.
+  Angular: '~7.1.0-beta.0',
+  RxJs: '~6.3.3',
+  ZoneJs: '~0.8.26',
+  TypeScript: '~3.1.6',
+  TsLib: '^1.9.0',
+  // The versions below must be manually updated when making a new devkit release.
+  DevkitBuildAngular: '~0.11.0-beta.0',
+  DevkitBuildNgPackagr: '~0.11.0-beta.0',
+};
